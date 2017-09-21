2454df4fdfa246ccc87637c136799c6b1bcb8b18
carlosv123 <vasquezcarlos1996@gmail.com>
2017-09-21 11:48:00 -0700

    initial commit

---
+++ CustomListProject/CustomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListProject
+{
+    public class CustomList<T>
+    {
+        T[] items;
+        public CustomList()
+        {
+            items = new T[1];
+        }
+        public void Add(T item)
+        {
+
+        }
+    }
+} 
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ddOneNumber()
+        {
+            //arrange
+            CustomList<int> list = new CustomList<int>();
+            int expected = 16;
+            //customlist = new customList
+
+            // act
+
+            list.Add(16);
+
+            //assert
+            Assert.AreEqual(expected, list[0]);
+        }
+
+    }
+}
+
